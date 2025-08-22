79200bcea55b3fe855570b19925303de5ec2bf2a
samer alkhatib <conan.500@hotmail.com>
2025-08-22 11:02:06 +0300

    Creach coaches list

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,8 +5,49 @@
       <button>Refresh</button>
       <router-link to="/register">Register as coach</router-link>
     </div>
-    <ul>
-      LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :areas="coach.areas"
+        :description="coach.description"
+        :rate="coach.hourlyRate"
+      ></coach-item>
     </ul>
+    <h2 v-else>No coaches found.</h2>
   </section>
 </template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
+export default {
+  components: {
+    CoachItem,
+  },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
