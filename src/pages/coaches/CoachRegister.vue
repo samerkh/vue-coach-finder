8180ca12440728f5526bfd192c8165603506ff0e
samer alkhatib <conan.500@hotmail.com>
2025-08-23 06:48:18 +0300

    Create coach form page

--- src/pages/coaches/CoachRegister.vue
+++ src/pages/coaches/CoachRegister.vue
@@ -1,1 +1,16 @@
-<template>Register works</template>
+<template>
+  <base-card>
+    <h2>Register as a coach</h2>
+    <coach-form></coach-form>
+  </base-card>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
