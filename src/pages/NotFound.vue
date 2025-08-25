3261715a320bdf85a76eb9296401b1a1f359f3c2
samer alkhatib <conan.500@hotmail.com>
2025-08-25 20:30:14 +0300

    Update not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,1 +1,9 @@
-<template>not found works</template>
+<template>
+  <base-card>
+    <h2>Page not found</h2>
+    <p>
+      This page could not be found - maybe check all our
+      <router-link to="/coaches">Coaches</router-link>.
+    </p>
+  </base-card>
+</template>
