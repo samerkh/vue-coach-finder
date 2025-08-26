a9c61ae38bd559e620f24837fecddb0d9f4282b6
samer alkhatib <conan.500@hotmail.com>
2025-08-26 23:36:16 +0300

    Fix webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const webpack = require('webpack');
+
 module.exports = {
   transpileDependencies: ['birpc'],
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false', // disable hydration mismatch debug in prod
+        __VUE_OPTIONS_API__: 'true', // keep Options API enabled
+        __VUE_PROD_DEVTOOLS__: 'false', // disable devtools in prod
+      }),
+    ],
+  },
 };
